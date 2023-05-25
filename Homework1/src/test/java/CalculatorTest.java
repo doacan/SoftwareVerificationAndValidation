2f85eb7023797e77f7eaf3636c5dc3e99cedb6d7
doacan <93758000+doacan@users.noreply.github.com>
2023-05-25 15:43:55 +0300

    new

--- Homework1/src/test/java/CalculatorTest.java
+++ Homework1/src/test/java/CalculatorTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 // Student surname: Karaman
 // Course name: SOFTWARE VERIFICATION AND VALIDATION
 // Homework number: HOMEWORK 1
-// Repository address:
+// Repository address: https://github.com/doacan/SoftwareVerificationAndValidation
 
 class CalculatorTest {
 
@@ -38,12 +38,10 @@ class CalculatorTest {
     // while first 5 tests are testing for EQUALITY of two objects.
     @Test
     void testDivision6() {
-
         Exception exception = assertThrows(
                 IllegalArgumentException.class,
                 () -> Calculator.divide(12.5, 0),
                 "IllegalArgumentException expected."
         );
-
     }
 }
