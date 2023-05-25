bff9bb2a2d8f7356bffc08c3bd836bd5d0307f1a
doacan <93758000+doacan@users.noreply.github.com>
2023-05-25 15:47:01 +0300

    new

--- Homework1/src/test/java/CalculatorTest.java
+++ Homework1/src/test/java/CalculatorTest.java
@@ -6,13 +6,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-// Student id: B211202403
-// Student name: Doğa Can
-// Student surname: Karaman
-// Course name: SOFTWARE VERIFICATION AND VALIDATION
-// Homework number: HOMEWORK 1
-// Repository address: https://github.com/doacan/SoftwareVerificationAndValidation
-
 class CalculatorTest {
 
     @BeforeEach
@@ -33,9 +26,7 @@ class CalculatorTest {
     void testDivision(double a, double b, double expected) {
         assertEquals(expected, Calculator.divide(a, b));
     }
-
-    //The last test method "testDivision6" is testing for an EXCEPTION...
-    // while first 5 tests are testing for EQUALITY of two objects.
+    
     @Test
     void testDivision6() {
         Exception exception = assertThrows(
